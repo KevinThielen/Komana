af1a65f79cc5a2025439738f7fb645e0f0753e20
kevin <kevin@crunchbang>
2013-12-13 20:28:29 +0100

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Komana::Application.config.secret_key_base = '3d8711189d08e44f6efbdb0abea2c6d9579787e307644c184720814e53d5d2e65653374415347ad25267e0a2537127c4d79652f148de480aabd4043d09199eab'
