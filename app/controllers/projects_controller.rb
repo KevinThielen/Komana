47eb228fd36c01e2470821c49e6115c010e8d40a
kevin <kevin@crunchbang>
2013-12-18 17:17:21 +0100

    Added Projects-Controller and implemented Create and Read

---
+++ app/controllers/projects_controller.rb
@@ -0,0 +1,27 @@
+class ProjectsController < ApplicationController
+	def new 
+		@project = Project.new
+	end
+	
+	def create
+		@project = Project.new(project_params)
+		if @project.save
+			redirect_to @project
+		else
+			render 'new'
+		end
+	end
+	
+	def show
+		@project = Project.find(params[:id])
+	end
+	
+	def index
+		@projects = Project.all
+	end
+	
+private
+	def project_params
+		params.require(:project).permit(:name)
+	end
+end
