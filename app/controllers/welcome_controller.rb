14478eb100c1b20eb9ac19932b483dd7d15259c0
Michael Landreh <michael.landreh@gmail.com>
2014-01-06 14:03:21 +0100

    Add a Link to User Profil Edit Path

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,9 @@
 class WelcomeController < ApplicationController
   def index
+    #if signed_in?
+            #render 'show_with_edit'
+       #else
+            #render 'show_without_edit`
+       #end
   end
 end
