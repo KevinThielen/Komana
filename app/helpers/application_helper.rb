e2e611ed037a5bceb74747b4bbdb3d7dd89c0e35
Michael Landreh <michael.landreh@gmail.com>
2014-01-10 14:15:22 +0100

    Fixed bug with german translation file

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,8 +7,8 @@ module ApplicationHelper
     flash[:alert]
   end
   
-  def avatar_url(user)
+  def avatar_url(user, size)
     gravatar_id = Digest::MD5::hexdigest(user.email).downcase
-    "http://gravatar.com/avatar/#{gravatar_id}.png?s=50"
+    "http://gravatar.com/avatar/#{gravatar_id}.png?s=" + size.to_s
   end
 end
