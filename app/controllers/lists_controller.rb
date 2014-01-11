faa0032d695b70336763a009b7eb0f08dbcf9163
kevin <kevin@crunchbang>
2014-01-11 22:59:07 +0100

    Removed List form and added new List

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -2,8 +2,9 @@ class ListsController < ApplicationController
   before_filter :authenticate_user!
 	def create
 		@project = Project.find(params[:project_id])
-		@list = @project.lists.create(params[:list].permit(:name))
-			redirect_to project_path(@project)
+		@list = @project.lists.create(:name => "neue Liste")
+		
+		redirect_to project_path(@project)
 	end
 	
 	def show	
