3f7b6c323311ee34bfe6c3865b682c5f02a56e79
Sebastian Schmitt <student@student.(none)>
2014-01-22 17:01:00 +0100

    added priority

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,6 +1,6 @@
 class Task < ActiveRecord::Base
 
-	#validates :priority, :inclusion => { :in => %w{low medium high} }
+	validates :priority, :inclusion => { :in => %w{low medium high} }
   
   	belongs_to :list
   	has_and_belongs_to_many :users
