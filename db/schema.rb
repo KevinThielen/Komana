9bf01f6b5b96ba9b8b1bbd0e54a30fe826a737f2
Michael Landreh <michael.landreh@gmail.com>
2014-02-08 15:16:18 +0100

    Added Redcar to git ignore

--- db/schema.rb
+++ db/schema.rb
@@ -142,4 +142,11 @@ ActiveRecord::Schema.define(version: 20140121154945) do
   add_index "users", ["email"], name: "index_users_on_email", unique: true
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
 
+  create_table "users_projects", force: true do |t|
+    t.integer  "project_id"
+    t.integer  "user_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
 end
