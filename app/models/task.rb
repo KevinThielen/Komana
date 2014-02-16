9a53407e48a17247845853bac43c75976cc33c89
Sebastian Schmitt <student@student.(none)>
2014-02-16 14:26:59 +0100

    added profil change validations

--- app/models/task.rb
+++ app/models/task.rb
@@ -4,11 +4,9 @@ class Task < ActiveRecord::Base
   has_and_belongs_to_many :users
 
   validates :name, presence: true, length: { minimum: 3 }
+  validates :priority, :inclusion => { :in => %w{low medium high} }, :allow_nil => true
   
   #include PublicActivity::Model
 	# tracked
-	 
-
-	validates :priority, :inclusion => { :in => %w{low medium high} }
   
 end
