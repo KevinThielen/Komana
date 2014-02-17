309631c1a6374fe7acdc3bc41bdbb08bc8d56ce7
Sebastian Schmitt <student@student.(none)>
2014-02-17 13:18:23 +0100

    final validations

--- app/models/task.rb
+++ app/models/task.rb
@@ -3,7 +3,8 @@ class Task < ActiveRecord::Base
   belongs_to :list
   has_and_belongs_to_many :users
 
-  validates :name, presence: true, length: { minimum: 3 }
+  validates :titel, presence: true, length: { minimum: 3 }
+  validates :text, length: { maximum: 512 }
   validates :priority, :inclusion => { :in => %w{low medium high} }, :allow_nil => true
   
   #include PublicActivity::Model
