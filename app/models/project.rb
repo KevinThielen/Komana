2b34b023f4167dccfd6a205f8d84f8978255ed94
Sebastian Schmitt <student@student.(none)>
2014-02-18 13:01:09 +0100

    added last_updated_at logic

--- app/models/project.rb
+++ app/models/project.rb
@@ -7,4 +7,22 @@ class Project < ActiveRecord::Base
 	
 	 #include PublicActivity::Model
 	 #tracked
+
+	 def get_last_updated
+
+	 	last_updated_list = List.where("project_id = ?", id).order(updated_at).first
+	 	last_updated_list_for_tasks = List.where("project_id = ?", id)
+
+	 	last_updated_list_for_tasks.each do |last_updated|
+
+		 		last_updated_task = last_updated.tasks.order(updated_at).first
+
+		 	if last_updated_task.updated_at >= last_updated_list.updated_at
+		 		@last_updated_project = last_updated_task.updated_at
+		 	else
+		 		@last_updated_project = last_updated_list.updated_at
+		 	end
+		end
+	 	return @last_updated_project
+	 end
 end
