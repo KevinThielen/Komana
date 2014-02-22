f2406241a8a63ffb43c033111b13b5c170edcd28
Sebastian Schmitt <student@student.(none)>
2014-02-22 15:22:33 +0100

    Urgent tasks is now displayed properly

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,15 +1,12 @@
 class WelcomeController < ApplicationController
   before_filter :authenticate_user!, except: [:index]
-  
+
   def index
     if signed_in?
         redirect_to welcome_dashboard_path
     end
     
   end
-  
-
-  
 end
 
 
