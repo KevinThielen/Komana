419b0582ddc948dbbac19630296f45f59d3342e5
kevin <kevin@crunchbang.(none)>
2014-02-26 13:47:25 +0100

    small fixes

--- app/models/project.rb
+++ app/models/project.rb
@@ -12,20 +12,20 @@ class Project < ActiveRecord::Base
 	end
 	
 	
-	def get_last_updated(id)
+	def get_last_updated
 
 	 	@project = Project.find(id)
-	 	last_updated_list = List.where("project_id = ?", id).order(updated_at).first
+	 	last_updated_list = List.where("project_id = ?", id).order(:updated_at).first
 	 	last_updated_lists_for_tasks = List.where("project_id = ?", id)
 
 		 	last_updated_lists_for_tasks.each do |last_updated|
 
-				 		last_updated_task = last_updated.tasks.order(updated_at).first
+				 		last_updated_task = last_updated.tasks.order(:updated_at).first
 
 				if last_updated_task.nil?
 				
 					elsif last_updated_task.updated_at >= last_updated_list.updated_at && last_updated_task.updated_at > @project.updated_at
-					 		@project.updated_at = last_updated_task.updated_at
+					 	@project.updated_at = last_updated_task.updated_at
 					end
 				if last_updated_list.updated_at > @project.updated_at
 				 		@project.updated_at = last_updated_list.updated_at
